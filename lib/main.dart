c74e31814ecdaf50a78050299de642aace9150f1
enad123 <72962012+enad123@users.noreply.github.com>
2020-10-16 12:49:07 +0700

    Update main.dart
    buat hacktober fest

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+//buat hacktober fest
+
 void main() => runApp(QuizApp());
 
 class QuizApp extends StatelessWidget {
